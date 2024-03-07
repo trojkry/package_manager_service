249fd08a297350bd8bf0361a9e72797ca99bfd3d
trojkry <krystof.trojak@gmail.com>
2024-03-07 12:18:12 +0100

    first test

---
+++ main.py
@@ -0,0 +1,17 @@
+# importy
+from flask import Flask
+from app.routes import install, update, uninstall, status, dependencies
+
+# Vytvoření instance Flask aplikace
+app = Flask(__name__)
+
+# Přidání blueprintů (modulů) pro jednotlivé části API
+app.register_blueprint(install.bp)
+##app.register_blueprint(update.bp)
+##app.register_blueprint(uninstall.bp)
+##app.register_blueprint(status.bp)
+##app.register_blueprint(dependencies.bp)
+
+# Spuštění aplikace
+if __name__ == '__main__':
+    app.run(debug=True)
