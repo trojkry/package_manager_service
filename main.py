ede5b2dc12e3f877f302d49970775b238baf6b4b
trojkry <krystof.trojak@gmail.com>
2024-03-13 13:06:11 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # importy
 from flask import Flask
-from app.routes import install, update, uninstall, status, dependencies
+from app.routes import install, update, uninstall, status, dependencies, list
 
 # Vytvoření instance Flask aplikace
 app = Flask(__name__)
 
 # Přidání blueprintů (modulů) pro jednotlivé části API
 app.register_blueprint(install.bp)
+app.register_blueprint(list.bp)
 #app.register_blueprint(update.bp)
 #app.register_blueprint(uninstall.bp)
 #app.register_blueprint(status.bp)
