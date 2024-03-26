c01a2c38af50a434214643523af3cad322611750
trojkry <krystof.trojak@gmail.com>
2024-03-26 09:05:01 +0100

    update main.py, add list.py, search.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from app.routes.install import install
 from app.routes.update import update
 from app.routes.upgrade import upgrade
 from app.routes.remove import remove
-
+from app.routes.list import list
+from app.routes.search import search
 
 app = Flask(__name__)
 
@@ -11,6 +12,8 @@ app.register_blueprint(install)
 app.register_blueprint(update)
 app.register_blueprint(upgrade)
 app.register_blueprint(remove)
+#app.register_blueprint(list)
+#app.register_blueprint(search)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
