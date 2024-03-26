9b83a1e45e9222e728da453a885d6c2edc828d28
trojkry <krystof.trojak@gmail.com>
2024-03-26 09:01:09 +0100

    update main.py, config.json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.routes.install import install
 from app.routes.update import update
 from app.routes.upgrade import upgrade
+from app.routes.remove import remove
 
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(install)
 app.register_blueprint(update)
 app.register_blueprint(upgrade)
+app.register_blueprint(remove)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
